44dcd9b2434d87b4f43192a66dde136440079985
mallika <mallikapk@sternadevices.in>
2024-07-23 13:40:30 +0530

    Basic driver configuration setup

---
+++ U3_UAT/src/main/java/U3basesetup/U3basesetup.java
@@ -0,0 +1,10 @@
+package U3basesetup;
+
+public class U3basesetup {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ U3_UAT/src/main/java/webElements/Webelements.java
@@ -0,0 +1,10 @@
+package webElements;
+
+public class Webelements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
